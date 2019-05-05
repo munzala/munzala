71f1006cb6f97a09b139f989caa6e5430a18b71f
Geoffrey Huntley <ghuntley@ghuntley.com>
2019-05-05 18:59:10 +1000

    s

---
+++ terraform/env-hq/main.tf
@@ -0,0 +1,39 @@
+# Copyright (c) 2019 Geoffrey Huntley. All rights reserved.
+# SPDX-License-Identifier: AGPL-3.0
+
+# PRODUCTION
+
+locals {
+  tld  = "${var.env}.${var.fqdn}"
+  fqdn = "${var.stack}.${local.tld}"
+}
+
+data "aws_availability_zones" "available" {}
+
+data "aws_region" "current" {
+}
+
+data "aws_region" "us-east-1" {
+  name = "us-east-1"
+}
+
+data "aws_region" "us-west-1" {
+  name = "us-west-1"
+}
+
+data "aws_route53_zone" "zone" {
+  name = "${local.tld}." # feel free to make this lookup more sophisticated
+}
+
+data "aws_acm_certificate" "cloudfront" {
+  domain   = "${local.tld}"
+  statuses = ["ISSUED"]
+  provider = "aws.us-east-1"
+}
+
+data "aws_acm_certificate" "cert" {
+  domain   = "${local.tld}"
+  statuses = ["ISSUED"]
+}
+
+
---
+++ terraform/env-hq/terraform.tfvars
@@ -0,0 +1,9 @@
+# PRODUCTION
+
+env = "prod"
+
+fqdn = "asics.digital"
+
+cloudfront_origin_access_identity_path = "origin-access-identity/cloudfront/E242SD4F68N60C"
+
+price_class = "PriceClass_All"
